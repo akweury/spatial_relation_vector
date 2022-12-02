ec34160758428dab63d71c537634c47355aebc1d
shaji-tp <jingyuan.sha@tu-darmstadt.de>
2022-12-02 11:20:15 +0100

    init commit

--- test.py
+++ test.py
@@ -1,26 +1,13 @@
 # Created by shaji at 02.12.2022
 
 import torchvision
+import pycocotools
+
 from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 from torchvision.models.detection import FasterRCNN
 from torchvision.models.detection.rpn import AnchorGenerator
 
-model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True)
-model.eval()
+object_detection_model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=True, progress=False)
+object_detection_model.eval()
 
-
-def get_prediction(img_path, threshold):
-  img = Image.open(img_path)
-  transform = T.Compose([T.ToTensor()])
-  img = transform(img)
-  pred = model([img])
-  pred_score = list(pred[0]['scores'].detach().numpy())
-  pred_t = [pred_score.index(x) for x in pred_score if x>threshold][-1]
-  masks = (pred[0]['masks']>0.5).squeeze().detach().cpu().numpy()
-  pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())]
-  pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())]
-  masks = masks[:pred_t+1]
-  pred_boxes = pred_boxes[:pred_t+1]
-  pred_class = pred_class[:pred_t+1]
-  return masks, pred_boxes, pred_class
