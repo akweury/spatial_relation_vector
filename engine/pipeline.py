18c641b399de255c6769f8f27517ab97bec0f876
jing <shajingyuan@me.com>
2022-12-13 10:31:33 +0100

    fix a bug for picking colors

--- engine/pipeline.py
+++ engine/pipeline.py
@@ -287,8 +287,6 @@ class LogManager():
         img_annot_labels = [f"{categories[0][label]}: {prob:.2f}" for label, prob in
                             zip(img_labels, img_preds[0]["scores"].detach().to("cpu").numpy())]
         colors = [config.colors[i] for i in img_labels]
-
-        colors=config.colors[:len(categories)]
         img_output_tensor = draw_bounding_boxes(image=img_tensor_int[0],
                                                 boxes=img_preds[0]["boxes"],
                                                 labels=img_annot_labels,
