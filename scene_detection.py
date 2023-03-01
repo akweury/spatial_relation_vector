c199bfc96e55b9e790b60b62baed69ec3097f6f7
jing <shajingyuan@me.com>
2023-03-01 19:33:55 +0100

    update dataset

--- scene_detection.py
+++ scene_detection.py
@@ -29,7 +29,7 @@ pos_loader = DataLoader(pos_dataset, shuffle=True, batch_size=args.batch_size, c
 neg_loader = DataLoader(neg_dataset, shuffle=True, batch_size=args.batch_size, collate_fn=pipeline.collate_fn)
 
 neg_pred = torch.zeros(size=(pos_dataset.__len__(), 6, 8))
-pos_pred = torch.zeros(size=(pos_dataset.__len__(), 6, 8))
+pos_pred = torch.zeros(size=(neg_dataset.__len__(), 6, 8))
 
 categories = config.categories
 
