44bf7c027b35cfb3a3ca6bb86ea2de2610224a1f
jing <shajingyuan@me.com>
2023-04-11 12:27:46 +0200

    fix a bug

--- scene_detection.py
+++ scene_detection.py
@@ -69,6 +69,8 @@ for data_type in ['train', 'val', "test"]:
             # fact extractor
             continual_spatial_objs = rule_utils.get_continual_spatial_objs(f"negative_{i}", od_prediction, images,
                                                                            vertex, objects, log_manager)
+            if continual_spatial_objs is None:
+                continue
             # [x,y,z, color1, color2, color3, shape1, shape2, conf]
             neg_pred[i, :] = scene_detection_utils.obj2tensor(continual_spatial_objs[0][:max_obj_num], max_obj_num)
 
