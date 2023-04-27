3f8bb52867a00c1789562417d021582f8f5d5766
jing <shajingyuan@me.com>
2023-04-27 14:53:54 +0200

    save screen position of each predictions

--- scene_detection_utils.py
+++ scene_detection_utils.py
@@ -1,7 +1,7 @@
 import torch
 
 
-def obj2tensor(obj_predictions,max_obj_num):
+def obj2tensor(obj_predictions, max_obj_num):
     obj_tensor = torch.zeros(size=(max_obj_num, 9))
     for obj_i, obj in enumerate(obj_predictions):
         obj_tensor[obj_i, 0:3] = torch.from_numpy(obj.position)
@@ -16,4 +16,5 @@ def obj2tensor(obj_predictions,max_obj_num):
         elif obj.shape == "cube":
             obj_tensor[obj_i, 7] = 1
         obj_tensor[obj_i, 8] = obj.pred
+        obj_tensor[obj_i, 9:11] = obj.screenPosition
     return obj_tensor
